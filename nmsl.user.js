ce8bb69fdae231c4f3507f22c411ee5b03aacd49
Lictex_ <lictex_steaven@outlook.com>
2018-12-20 11:06:52 +0800

    sb

--- nmsl.user.js
+++ nmsl.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         你忙啥了.js
 // @namespace    https://lictex.pw/
-// @version      0.1
+// @version      0.12
 // @description  nmsl
 // @author       lictex_
 // @match        http://study.zhihuishu.com/learning/videoList*
@@ -11,10 +11,13 @@
 
 (function() {
     'use strict';
-    $("body").prepend('<div style="width:calc(100% - 16px);height:32px;background:white;border:8px solid red;text-align:center;color:red;font-size:20px;font-weight:bold;"><span id="nmsl" href="https://github.com/xyning/ZhiHuiShu-Assistant">NMSL is loading.</span></div>');
-    (async function(){
-        var script=$.ajax({url:"https://raw.githubusercontent.com/xyning/ZhiHuiShu-Assistant/master/main.js",async:false}).responseText;
-        $("#nmsl").text("NMSL is running.");
-        eval(script);
-    })();
+    $("body").prepend('<div style="width:calc(100% - 16px);height:32px;background:white;border:8px solid red;text-align:center;color:red;font-size:20px;font-weight:bold;"><span id="nmsl">NMSL is loading.</span></div>');
+    $.ajax({
+        url:"https://raw.githubusercontent.com/xyning/ZhiHuiShu-Assistant/master/main.js", 
+        async:true,
+        success:function(data){
+            $("#nmsl").text("NMSL is running.");
+            eval(data.responseText);
+        }
+    });
 })();
